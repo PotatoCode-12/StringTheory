7f46bd9e8f15caff8a6445dfec3d24a864eafab2
PotatoCode-12 <josephcorigs@gmail.com>
2026-01-03 11:20:51 -0500

    updating file

---
+++ operations.py
@@ -0,0 +1,45 @@
+import math
+from scipy.integrate import quad
+import numpy as np 
+from sympy import symbols, diff,integrate, Function, Derivative, summation, oo, Abs
+import sympy as sp
+import variables
+import constants
+import actions
+import algebra
+import amplitudes
+import bounds
+import communicators
+import compactification
+import conjugates
+import divergences
+import energies
+import expansions
+import fields
+import folding
+import functions
+import gauges
+import general
+import ghosts
+import hamiltonians
+import implications
+import invarianceconditions
+import lagrangians
+import metrics
+import motion
+import operators
+import particles
+import pathintegrals
+import relations
+import rescaling
+import space
+import spin
+import states
+import symmetries
+import tensions
+import tetrads
+import transformations
+import vacuum
+import waves
+import weights
+
